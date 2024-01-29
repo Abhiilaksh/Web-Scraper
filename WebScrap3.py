c0b19fe933a1998db8eb84e2fb3f81ebc8cb5e3b
Abhilaksh Thakur <135262451+Abhiilaksh@users.noreply.github.com>
2024-01-29 23:56:15 +0530

    Update WebScrap3.py

--- WebScrap3.py
+++ WebScrap3.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-source=requests.get('https://timesofindia.indiatimes.com/india/union-minister-says-caa-will-be-implemented-in-one-week-mamata-banerjee-hits-back/articleshow/107229633.cms').text
-#it returns the html file
-soup=BeautifulSoup(source, 'lxml')    #lxml is a parser
-for heading in soup.find_all('h1'):   #it returns list of all headings
-    print(heading.text)               #it prints the first heading
-    print()
-for para in soup.find_all('div' , class_='_s30J clearfix'):
-    print(para.text)
-    print()
+import csv
+with open('output.csv', 'w', encoding='utf-8')as file:         #it creates a csv file
+    writer=csv.writer(file)                                    #it writes in the csv file
+    writer.writerow(['URL','Heading'])                         #it writes the heading of the columns
+    with open('input.csv', 'r', encoding='utf-8')as file:      #it opens the csv file
+        reader=csv.reader(file)                                #it reads the csv file                           
+        for row in reader:                                     #it reads the rows of the csv file                 
+            source=requests.get(row[0]).text                   #it returns the html file
+            soup=BeautifulSoup(source, 'lxml')                 #it returns the html file in a readable format
+            for heading in soup.find_all('h1'):
+                print(heading.text)
+                writer.writerow([row[0],heading.text])         #it writes the url and heading in the csv file
+                print()      
