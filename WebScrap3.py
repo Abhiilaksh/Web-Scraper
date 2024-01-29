9453e40c06c244ff6d7b02c773030f572a5d343b
Abhilaksh Thakur <135262451+Abhiilaksh@users.noreply.github.com>
2024-01-29 20:19:59 +0530

    Update WebScrap3.py

--- WebScrap3.py
+++ WebScrap3.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 source=requests.get('https://timesofindia.indiatimes.com/india/union-minister-says-caa-will-be-implemented-in-one-week-mamata-banerjee-hits-back/articleshow/107229633.cms').text
-soup=BeautifulSoup(source, 'lxml')
-for heading in soup.find_all('h1'):
-    print(heading.text)
+#it returns the html file
+soup=BeautifulSoup(source, 'lxml')    #lxml is a parser
+for heading in soup.find_all('h1'):   #it returns list of all headings
+    print(heading.text)               #it prints the first heading
     print()
 for para in soup.find_all('div' , class_='_s30J clearfix'):
     print(para.text)
